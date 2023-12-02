9304f11a912d67d007d6fd092f1fb647ad7bfa45
JiaXing-Cheng <55724384+JiaXing-Cheng@users.noreply.github.com>
2023-12-02 13:45:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-12-12' },
+        { keyword: 'love_day', date: '2099-12-12' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2024-05-01' },
       ],
