6efcd46770bb71628edea157eb0297a6eff7cabe
JiaXing-Cheng <55724384+JiaXing-Cheng@users.noreply.github.com>
2023-12-04 08:13:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTHbO6b4VExhySFqVer4nkJ59RwI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2spmrgl00rBjdp38N93TyWSjYwWa9o_WK2Tkf4N3FWo',
+      useTemplateId: 'VVkgM3o_X-QIB4uYinf4wLKfH-J72wyzEvZqHtmFLNg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
